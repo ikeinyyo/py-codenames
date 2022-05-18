fafe5d2a7d08b88658493e4b5cb8417b18108e93
ikeinyyo <ikeinyyo@outlook.com>
2022-05-18 22:15:15 +0200

    Add 2 bots support
    Refactor
    
    Refactor classes
    
    Refactor messages
    
    Change texts

--- src/bots/BotBase.py
+++ src/bots/BotBase.py
@@ -2,16 +2,24 @@ from abc import ABC, abstractmethod
 
 
 class BotBase(ABC):
-    def __init__(self):
+    def __init__(self, is_captain=False):
         self._board = None
+        self.is_captain = is_captain
+        self.is_red = None
 
     def set_board(self, board):
         self._board = board
 
+    def set_blue_team(self):
+        self.is_red = False
+
+    def set_red_team(self):
+        self.is_red = True
+
     @abstractmethod
-    def give_clue(self, is_red_turn):
+    def give_clue(self):
         pass
 
     @abstractmethod
-    def give_answer(self, is_red_turn):
+    def give_answer(self):
         pass
