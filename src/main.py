5b74acb955575f6ad2309c9c3ac13497a18f3e0e
Sergio Gallardo Sales <ikeinyyo@outlook.com>
2022-06-01 12:32:22 +0200

    feat(app): Add logs to show the game steps (#7)

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from bots.DistanceBot import DistanceBot
 from codenames import Codenames
 
 if __name__ == '__main__':
-    red_bot = DistanceBot(is_captain=True, is_team_member=True)
+    red_bot = DistanceBot(is_team_member=True, is_captain=True)
     blue_bot = DistanceBot(is_team_member=True)
     codenames = Codenames(
         red_bot=red_bot, blue_bot=blue_bot)
