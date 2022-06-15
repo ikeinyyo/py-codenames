ad189e304e887091b9613e56b73c967e05862880
Jose Manuel Pinto Lozano <josemanuelpintolozano@gmail.com>
2022-06-16 00:07:13 +0200

    Solved endless loop in the refactor

--- src/bots/HypernymBot.py
+++ src/bots/HypernymBot.py
@@ -37,19 +37,19 @@ class HypernymBot(BotBase):
        
     def __get_distance_to_first_common_hypernym_aux(self, hypernyms1, hypernyms2, word1, word2):
         ## Base case
-        if len(hypernyms1.intersection(hypernyms2)) > 0 or not (self.__has_new_hypernyms(hypernyms1) or self.__has_new_hypernyms(hypernyms2)):
+        if len(set(hypernyms1).intersection(hypernyms2)) > 0 or not (self.__has_new_hypernyms(hypernyms1) or self.__has_new_hypernyms(hypernyms2)):
             hypernyms = list(set(hypernyms1).intersection(hypernyms2))
             distance_to_first_word = list(map(lambda x: max(list(map(lambda y: self.__get_path_similarity(x, y), wn.synsets(word1, lang=self.__lang)))), hypernyms))
             distance_to_second_word = list(map(lambda x: max(list(map(lambda y: self.__get_path_similarity(x, y), wn.synsets(word2, lang=self.__lang)))), hypernyms))
             possible_hypernyms = zip(np.add(distance_to_first_word, distance_to_second_word), distance_to_first_word, distance_to_second_word)
             max_value = max(possible_hypernyms, key=itemgetter(0), default=(0,0,0))
-            return (max_value[1], max_value[2])   
+            return [max_value[1], max_value[2]]   
         
-        return self.__get_distance_to_first_common_hypernym_aux(hypernyms1=self.__get_hypernyms_of_synsets(hypernyms1), hypernyms2=self.__get_hypernyms_of_synsets(hypernyms2), word1=word1, word2=word2)
+        return self.__get_distance_to_first_common_hypernym_aux(hypernyms1=list(set(hypernyms1 + self.__get_hypernyms_of_synsets(hypernyms1))), hypernyms2=list(set(hypernyms2 + self.__get_hypernyms_of_synsets(hypernyms2))), word1=word1, word2=word2)
 
     
     def __has_new_hypernyms(self, hypernyms_set):
-        return len(hypernyms_set) != len(self.__get_hypernyms_of_synsets(hypernyms_set))
+        return len(hypernyms_set) != len(list(set(hypernyms_set + self.__get_hypernyms_of_synsets(hypernyms_set))))
 
     def __load_model(self):
         fasttext.util.download_model(
